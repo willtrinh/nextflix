927a70596caf07abfe83d02b4899d0c2a6ed402c
willtrinh <williamltrinh@gmail.com>
2021-12-14 11:56:11 -0800

    next-auth config

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,13 +8,13 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      // authorization: {
-      //   params: {
-      //     prompt: 'consent',
-      //     access_type: 'offline',
-      //     response_type: 'code',
-      //   },
-      // },
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
     }),
     // ...add more providers here
     GithubProvider({
