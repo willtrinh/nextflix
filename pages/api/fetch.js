14ef4c14b8ff6a2dd6a73ccedd9e086066625c7a
willtrinh <williamltrinh@gmail.com>
2021-12-13 19:43:02 -0800

    remove log

--- pages/api/fetch.js
+++ pages/api/fetch.js
@@ -5,7 +5,7 @@ async function handler(req, res) {
   const type = req.query;
   if (req.method === 'GET' && page < 5) {
     const data = await loadMore(page++, type);
-    console.log(data);
+
     res.status(200).json(data);
   } else {
     res.status(200).json({ message: 'end' });
